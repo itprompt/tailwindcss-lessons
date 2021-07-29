f8119e53ba9207b705f669b5f676b2f72df5916e
itprompt <itpromptdev@gmail.com>
2021-07-29 20:48:04 +0700

    add tailwindcss configuration

---
+++ tailwind.config.colors.js
@@ -0,0 +1,39 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            blueGray: colors.blueGray,
+            coolGray: colors.coolGray,
+            gray: colors.gray,
+            trueGray: colors.trueGray,
+            warmGray: colors.warmGray,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            sky: colors.sky,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
